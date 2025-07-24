8ee79e8db9b46cd67f35686d2fb3332b02f09f99
Jay1741 <j_darji233248@fanshaweonline.ca>
2025-07-24 18:45:34 -0400

    scripts/app.js index.html

--- scripts/app.js
+++ scripts/app.js
@@ -18,7 +18,7 @@ const app = Vue.createApp({
     // Define the methods for the app
     methods: {
         activeGasLevel(gasLevel) {
-            return this.gasLevelValue >= min & gasLevelValue<= max;
+            return this.gasLevelValue >= gasLevel & gasLevelValue<= gasLevel;
             // The logic goes here for if the gas level is within the level parameters return boolean
         }
     }
