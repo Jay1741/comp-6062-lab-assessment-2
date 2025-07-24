edf94be86595855ce7d7937b9e1ac61645c86231
Jay1741 <j_darji233248@fanshaweonline.ca>
2025-07-24 17:37:42 -0400

    Add files via upload

---
+++ scripts/app.js
@@ -0,0 +1,18 @@
+// Create a new Vue app
+const app = Vue.createApp({
+    // Define the data for the app
+    data() {
+        return {
+
+        };
+    },
+    // Define the methods for the app
+    methods: {
+        activeGasLevel(gasLevel) {
+            // The logic goes here for if the gas level is within the level parameters return boolean
+        }
+    }
+});
+
+// Mount the Vue app to the DOM
+app.mount('#app');
